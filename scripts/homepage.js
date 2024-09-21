6d3e790a0c8d0c32a4602995424026e20c96e74a
ChandlerMurphy <chandlermurphy02@gmail.com>
2024-09-21 09:15:04 -0600

    Added JS for Year and Updated elements. Also fixed CSS errors. I will do the JS array for course next and testing.

---
+++ scripts/homepage.js
@@ -0,0 +1,17 @@
+// Footer Current Year vvv
+const year = document.querySelector('#currentyear');
+const today = new Date();
+year.innerHTML = today.getFullYear();
+
+// Footer Last Modified vvv
+const lastUpdated = document.querySelector("#lastUpdated");
+lastUpdated.innerHTML = (document.lastModified);
+
+// Toggle for Hambuger Menu vvv
+const hamButton = document.querySelector("#hamburger");
+const ul = document.querySelector("ul");
+
+hamButton.addEventListener("click", () => {
+    ul.classList.toggle("open-nav");
+    hamButton.classList.toggle("open-ham");
+});
