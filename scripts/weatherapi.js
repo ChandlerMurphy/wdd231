b3117ba2bbec2de03204c0dc94eebf2e8957353c
ChandlerMurphy <chandlermurphy02@gmail.com>
2024-09-30 19:15:59 -0600

    Finished all the Learning Activities for W3 including Consuming APIs

--- scripts/weatherapi.js
+++ scripts/weatherapi.js
@@ -2,7 +2,11 @@ const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=eb728d14d471fc57db5219f45b06a8f2";
+const myKey = "eb728d14d471fc57db5219f45b06a8f2";
+const myLat = "49.75";
+const myLong = "6.64";
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
 
 async function apiFetch() {
     try {
@@ -10,14 +14,22 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
-            console.log(data.main.temp);
-            console.log(data.weather.0[description])
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
     } catch (error) {
         console.log(error);
     }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", data.weather[0].description);
+    captionDesc.textContent = `${desc}`;
 }
 
 apiFetch();
