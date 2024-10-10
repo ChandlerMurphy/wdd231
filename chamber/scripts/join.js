5245dcd05a2ebb8caf9baaa0a00622acc2c1072a
ChandlerMurphy <chandlermurphy02@gmail.com>
2024-10-10 15:00:07 -0600

    Split the constant JS code accross my website into its own JS and added the unique JS in its own file for both the Join and Thanks pages.

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,24 +1,5 @@
-// Toggle for Hambuger Menu vvv
-const hamButton = document.querySelector("#hamburger");
-const ul = document.querySelector("ul");
-
-hamButton.addEventListener("click", () => {
-    ul.classList.toggle("open-nav");
-    hamButton.classList.toggle("open-ham");
-});
-
-// Footer Current Year vvv
-const year = document.querySelector('#currentyear');
-const today = new Date();
-year.innerHTML = today.getFullYear();
-
-// Footer Last Modified vvv
-const lastUpdated = document.querySelector("#lastUpdated");
-lastUpdated.innerHTML = (document.lastModified); 
-
 // Member Cards Display and Modal Info
 import {membershipLevels} from "../data/member-levels.js";
-console.log(membershipLevels);
 
 const memberCards = document.querySelector("#member-cards");
 const dialogBox = document.querySelector("#dialog");
